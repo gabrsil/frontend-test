ba9e9ae5d51682aa2ebcf880739d2ebb016e362d
Gabriel <gabriel.aconsurgens@gmail.com>
2020-10-12 21:31:46 -0300

    add media queries

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
+import { Menu } from '@styled-icons/boxicons-regular';
 
 export const Container = styled.div`
     display: flex;
-
+    margin-top: 30px;
     img.header-logo {
         width: 122px;
         height: 47px;
     }
-    
+
     ul {
         list-style-type: none;
+    }
+
+    span.hamb-icon {
+        margin-left: auto;
+        cursor: pointer;
+        display: none;
+        @media (max-width: 780px) {
+            display: initial;
+        }
     }
 `;
 
@@ -18,28 +28,45 @@ export const NavBar = styled.ul`
     display: flex;
     align-items: center;
 
-`
+    @media (max-width: 780px) {
+        display: none;
+    }
+`;
+
+export const MenuIcon = styled(Menu)`
+    width: 40px;
+    height: 40px;
+    color: var(--main-lightgreen);
+`;
 
 export const NavItem = styled.li`
-
     margin: 0 35px;
     font-size: 1rem;
 
-    a {
-        
-        color: #4F5665;
+    a.nav-link {
+        color: #4f5665;
+        transition: filter 0.2s;
+
+        :hover {
+            filter: brightness(1.5);
+        }
     }
 
     a.nav-btn {
-        color: #00A536;
+        color: var(--main-lightgreen);
         border-radius: 20px;
-        border: 1px solid #00A536;
+        border: 1px solid var(--main-lightgreen);
         padding: 10px 30px;
         font-weight: bold;
+        transition: 0.2s;
+
+        :hover {
+            background-color: var(--main-lightgreen);
+            color: #fff;
+        }
     }
 
     :last-child {
         margin: 0px 15px;
     }
-
-`
+`;
