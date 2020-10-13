34756793c6d7003c15729aa54f4a9602d689bd84
Gabriel <gabriel.aconsurgens@gmail.com>
2020-10-12 21:31:19 -0300

    import SideBar component

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import Logo from '../../assets/logo 1.png'
-import { Container, NavBar, NavItem } from './styles';
+import React, { useState } from 'react';
+
+import Logo from '../../assets/logo 1.png';
+import { Container, NavBar, NavItem, MenuIcon } from './styles';
+import SideBar from '../SideBar';
 
 const Header: React.FC = () => {
-  return (
-    <Container>
-        <img src={Logo} alt="logo" className="header-logo"/>
-        <NavBar>
-            <NavItem>
-                <a href="">Sobre</a>
-            </NavItem>
-            <NavItem>
-                <a href="">Testemunhos</a> 
-            </NavItem>
-            <NavItem>
-                <a href="" className="nav-btn">Àrea do comerciante</a> 
-            </NavItem>
-        </NavBar>
-    </Container>
+    return (
+        <Container id="header-cont">
+            <img src={Logo} alt="logo" className="header-logo" />
+
+            <NavBar>
+                <NavItem>
+                    <a className="nav-link" href="">
+                        Sobre
+                    </a>
+                </NavItem>
+                <NavItem>
+                    <a className="nav-link" href="">
+                        Testemunhos
+                    </a>
+                </NavItem>
+                <NavItem>
+                    <a href="" className="nav-btn">
+                        Àrea do comerciante
+                    </a>
+                </NavItem>
+            </NavBar>
+            <SideBar />
+        </Container>
     );
-}
+};
 
 export default Header;
