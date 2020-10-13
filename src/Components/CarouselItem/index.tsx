ccea4c18448a31fb55c796bcdf68e33ba88d1156
Gabriel <gabriel.aconsurgens@gmail.com>
2020-10-12 21:29:38 -0300

    add class to h3

--- src/Components/CarouselItem/index.tsx
+++ src/Components/CarouselItem/index.tsx
@@ -3,32 +3,32 @@ import React from 'react';
 import { Container, StarIcon, PersonImage, PersonHeader } from './styles';
 
 interface IPersonParams {
-    id: string,
-    image: string,
-    firstname: string,
-    lastname: string,
-    website: string,
-    rate: number,
-    description: string
+    image: string;
+    firstname: string;
+    lastname: string;
+    website: string;
+    rate: number;
+    description: string;
 }
 
-const CarouselItem: React.FC<IPersonParams> = ({ image, firstname, lastname, website, rate, description, id }) => {
-  return (
-    <Container id={id}>
-        <PersonHeader>
-            <PersonImage image={image}/>
-            <section className="person-info">
-                <h3>{`${firstname} ${lastname}`}</h3>
-                <span className="person-website">{website}</span>
-            </section>
-            <section className="rate-area">
-                <span className="rate">{rate}</span>
-                <StarIcon/>
-            </section>
-        </PersonHeader>
+// eslint-disable-next-line react/prop-types
+const CarouselItem: React.FC<IPersonParams> = ({ image, firstname, lastname, website, rate, description }) => {
+    return (
+        <Container className="carousel-item">
+            <PersonHeader>
+                <PersonImage image={image} />
+                <section className="person-info">
+                    <h3 className="person-name">{`${firstname} ${lastname}`}</h3>
+                    <span className="person-website">{website}</span>
+                </section>
+                <section className="rate-area">
+                    <span className="rate">{rate}</span>
+                    <StarIcon />
+                </section>
+            </PersonHeader>
             <p className="person-description">{description}</p>
-    </Container>
+        </Container>
     );
-}
+};
 
 export default CarouselItem;
