88c5300d106123bd9a90a06ef71c1a7770bb74bb
Gabriel <gabriel.aconsurgens@gmail.com>
2020-10-12 21:32:11 -0300

    make Home Page Component

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import Ilustration1 from '../../assets/Illustration 1.svg';
+import CarouselItem from '../../Components/CarouselItem';
+import Carousel from '../../Components/Carousel';
+import BottomCard from '../../Components/BottomCard';
+import QuantitativeArea from '../../Components/QuantitativeArea';
+import MidContent from '../../Components/MidContent';
+
+import { Container, MainContent, FeedbackArea } from './styles';
+
+import Header from '../../Components/Header';
+
+interface IPerson {
+    firstname: string;
+    lastname: string;
+    website: string;
+    image: string;
+}
+
+const Home: React.FC = () => {
+    const [personList, setPersonList] = useState<IPerson[]>([]);
+
+    const fetchPersonApi = async () => {
+        fetch('https://fakerapi.it/api/v1/persons')
+            .then((res) => res.json())
+            .then((resp) => {
+                setPersonList(resp.data.slice(0, 9));
+            })
+            .catch((err) => console.log(err));
+    };
+    useEffect(() => {
+        fetchPersonApi();
+    }, []);
+
+    return (
+        <Container>
+            <Header />
+            <main>
+                <MainContent>
+                    <section className="content-info">
+                        <h1>
+                            Solicite entregadores de forma eficiente <span>🛵</span>
+                        </h1>
+                        <p>
+                            Somos a solução que o seu comércio precisa pra lidar com entregas de forma prática e
+                            objetiva.
+                        </p>
+                        <button className="contact-btn">Entre em contato</button>
+                    </section>
+                    <section className="content-img">
+                        <img src={Ilustration1} alt="" />
+                    </section>
+                </MainContent>
+                <QuantitativeArea />
+                <MidContent />
+                <FeedbackArea>
+                    <section className="text-area">
+                        <h1>
+                            Utilizada por centenas de empreendedores <span>👀</span>
+                        </h1>
+                        <p>
+                            Veja alguns depoimentos de comerciantes que utilizam a Loocal para gerenciar as demandas de
+                            entregas dos seus estabelecimentos
+                        </p>
+                    </section>
+                    <Carousel>
+                        {personList.map((person, id) => (
+                            <CarouselItem
+                                key={id}
+                                firstname={person.firstname}
+                                lastname={person.lastname}
+                                image={person.image}
+                                website={person.website}
+                                description="Lorem Ipsum este pur şi simplu o machetă 
+                        pentru text a industriei tipografice. Lorem Ipsum a 
+                        fost macheta standard a industriei încă din secolul al XVI-lea."
+                                rate={4.5}
+                            />
+                        ))}
+                    </Carousel>
+                </FeedbackArea>
+                <BottomCard />
+            </main>
+        </Container>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/styles.ts
@@ -0,0 +1,146 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 40px 60px;
+    min-height: 100vh;
+    background-color: #fff;
+
+    @media (max-width: 1440px) {
+        width: 100%;
+    }
+    @media (max-width: 1000px) {
+        padding: 0 40px;
+    }
+    @media (max-width: 700px) {
+        padding: 0 20px;
+    }
+`;
+export const MainContent = styled.div`
+    display: flex;
+    margin-top: 90px;
+    h1 {
+        font-size: 3.1rem;
+        font-weight: bold;
+        color: #0b132a;
+    }
+
+    section.content-info {
+        margin-right: 40px;
+
+        p {
+            line-height: 30px;
+            margin: 20px 0;
+            color: #4f5665;
+        }
+
+        button.contact-btn {
+            border: none;
+            background-color: #00a536;
+            color: #fff;
+            font-weight: bold;
+            padding: 10px 50px;
+            border-radius: 8px;
+            height: 50px;
+            box-shadow: 0px 30px 25px -15px rgba(0, 165, 54, 0.35);
+        }
+    }
+    section.content-img {
+        img {
+            width: 600px;
+        }
+    }
+
+    @media (max-width: 1300px) {
+        h1 {
+            font-size: 2.5rem;
+        }
+        section.content-img {
+            img {
+                width: 560px;
+                height: 280px;
+            }
+        }
+    }
+
+    @media (max-width: 1100px) {
+        h1 {
+            font-size: 2rem;
+        }
+        section.content-img {
+            img {
+                width: 540px;
+                height: 260px;
+            }
+        }
+    }
+
+    @media (max-width: 1000px) {
+        flex-direction: column-reverse;
+        align-items: center;
+        section.content-info {
+            margin: 0;
+            margin-top: 15px;
+            width: 500px;
+
+            p {
+                margin: 8px 0 25px;
+            }
+        }
+    }
+
+    @media (max-width: 600px) {
+        section.content-info {
+            width: 100%;
+            padding: 0 15px;
+            min-width: 320px;
+        }
+
+        section.content-img {
+            img {
+                width: 100%;
+                min-width: 320px;
+                height: 230px;
+            }
+        }
+    }
+`;
+export const FeedbackArea = styled.div`
+    margin: 120px 0;
+    width: 100%;
+    section.text-area {
+        text-align: center;
+        margin: 0 auto;
+        width: 500px;
+        margin-bottom: 50px;
+
+        h1 {
+            color: #0b132a;
+            font-weight: bold;
+            font-size: 1.8rem;
+            margin-bottom: 18px;
+            line-height: 40px;
+        }
+
+        p {
+            font-size: 1rem;
+            color: #4f5665;
+            line-height: 30px;
+        }
+
+        @media (max-width: 650px) {
+            width: 100%;
+        }
+
+        @media (max-width: 400px) {
+            h1 {
+                font-size: 1.6rem;
+            }
+
+            p {
+                font-size: 0.9rem;
+            }
+        }
+    }
+`;
